918951db077a89a9bedee2c8794fff5091004df1
William Watkins <wjwat@onionslice.org>
2022-01-12 12:32:52 -0600

    Add functionality to display a random number in a random shoutbox
    Co-authored-by: Daniel Lindsey <daniellindsey85254@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+function getRandomNum () {
+  return(Math.random());
+}
+
+let x = 0;
+
 $(document).ready(function() {
   $("#formOne").submit(function() {
     const person1Input = $("input#person1").val();
@@ -21,9 +27,30 @@ $(document).ready(function() {
 
   $('#shout-form').on('submit', function () {
     let newShout = $('input#user-shout').val().toUpperCase();
+    let cardID = "#card-" + Math.floor((Math.random() * 3)+1);
+    console.log(cardID)
+    $(cardID).text(newShout);
 
-    $('.card-body').text(newShout + Math.random());
+    /* Select each #card.card-body and change text to a random number
+    $('#card.card-body').each(function () {
+      $(this).text(getRandomNum());
+    });
+    */
 
+    /* do the same as above but in a worse way
+    $('.card-body').parent().each(function() {
+      console.log(`${x}`)
+      $(this).children('#card').text(getRandomNum());
+      x = x+1;
+    })*/
+
+    /* i forget what x had been set to, but this was wrong
+     * because we hadn't actually selected anything on subsequent runs
+    for(i = 0; i < x.length; i++) {
+      console.log("old: " + typeof(x[i]))
+      x[i].innerText = 'fart' + getRandomNum();
+      console.log("new: " + typeof(x[i]))
+    }*/
     event.preventDefault();
   });
 });
